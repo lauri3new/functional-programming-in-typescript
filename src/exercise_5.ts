8a7ddd1fbd79a963773c9d61a7d95837327e8480
Laurence Newman <laurence@wearepercent.com>
2019-12-20 18:32:46 +0000

    more streamz

--- src/exercise_5.ts
+++ src/exercise_5.ts
@@ -1,3 +1,4 @@
+import { Option, Some, None } from './Option'
 
 export const fiveOne = () => {
   interface Stream<A> {
@@ -24,7 +25,7 @@ export const fiveOne = () => {
     isEmpty: true
   })
 
-  console.log('stream 1', Stream(() => 2, () => Empty()))
+  // console.log('stream 1', Stream(() => 2, () => Empty()))
 
   const toList = <A>(a: Stream<A>): A[] => {
     if (a.isEmpty) {
@@ -33,5 +34,128 @@ export const fiveOne = () => {
     return [ a.head(), ...toList(a.tail()) ]
   }
 
-  console.log('stream 1', toList(Stream(() => 2, () => (Stream(() => 2, () => Empty())))))
+  // console.log('stream 1', toList(Stream(() => 2, () => (Stream(() => 2, () => Empty())))))
+  // five two
+
+  const take = <A>(n: number) => (as:Stream<A>): Stream<A> => {
+    if (n === 0) return Empty()
+    return Stream(as.head, () => take<A>(n -1)(as.tail()))
+  }
+
+  const takeFive = take(5)(Stream(() => 1, () => Stream(() => 2, () => Stream(() => 3, () => Stream(() => 4, () => Stream(() => 5, () => Stream(() => 6, () => Stream(() => 7, () => Empty<number>()))))))))
+  // console.log('takeFive', takeFive.head(), takeFive.tail())
+
+  const drop = <A>(n: number) => (as:Stream<A>): Stream<A> => {
+    if (n === 0) return as
+    return drop<A>(n -1)(as.tail())
+  }
+
+  const dropFive = drop(5)(Stream(() => 1, () => Stream(() => 2, () => Stream(() => 3, () => Stream(() => 4, () => Stream(() => 5, () => Stream(() => 6, () => Stream(() => 7, () => Empty<number>()))))))))
+  // console.log('dropFive', dropFive.head(), dropFive.tail())
+
+  const takeWhile = <A>(f: (_:A) => boolean) => (as: Stream<A>): Stream<A> => {
+    if (f(as.head())) {
+      return Stream(as.head, () => takeWhile(f)(as.tail()))
+    }
+    return Empty()
+  }
+
+  // console.log('takeWhile < 3', takeWhile((a: number) => a < 3)(Stream(() => 1, () => Stream(() => 2, () => Stream(() => 3, () => Empty())))).tail().tail().head())
+
+  const forAll = <A>(f: (_:A) => boolean) => (as: Stream<A>): boolean => {
+    if (as.isEmpty) {
+      return true
+    }
+    if (f(as.head())) { 
+      return forAll(f)(as.tail())
+    }
+    return false
+  }
+
+  console.log('forAll success', forAll((a: number) => a > 0)(Stream(() => 1, () => Stream(() => 2, () => Stream(() => 3, () => Empty())))))
+  console.log('forAll fail', forAll((a: number) => a < 2)(Stream(() => 1, () => Stream(() => 2, () => Stream(() => 3, () => Empty())))))
+
+  const foldRight = <A, B>(z:() => B) => (f: (_:A, __:() => B) => B) => (as: Stream<A>): (() => B) => {
+    if (as.isEmpty) return z
+    return () => f(as.head(), foldRight<A, B>(z)(f)(as.tail()))
+  }
+
+  console.log('foldRight stream 1', foldRight<number, number>(() => 0)((a: number, b: () => number) => a + b())(Stream(() => 1, () => Stream(() => 10, () => Empty())))())
+
+
+  const headOption = <A>(as: Stream<A>): Option<A> => {
+    if (as.isEmpty) {
+      return None()
+    }
+    return Some(as.head())
+  }
+
+  const headOptionI = <A>(as: Stream<A>): Option<A> => {
+    return foldRight<A, Option<A>>(() => {
+      if (as.isEmpty) return None()
+      return Some(as.head())
+    })((a: A, b: () => Option<A>) => b().flatMap(ib => Some(a)))(as.tail())()
+  }
+
+  console.log('non empety head', headOptionI(Stream(() => 1, () => Empty())))
+  console.log('empty head', headOptionI(Empty()))
+
+  const map = <A, B>(f:(_:A) => B) => (as: Stream<A>): () => Stream<B> => foldRight<A, Stream<B>>(
+    () => Empty()
+  )
+  (
+    (a, b) => Stream(() => f(a), b)
+  )
+  (
+    as
+  )
+
+  console.log('mappa', map((a: number) => a + 1)(Stream(() => 1, () => Empty()))().head())
+  const filter = <A>(f:(_:A) => boolean) => (as: Stream<A>): () => Stream<A> => foldRight<A, Stream<A>>(
+    () => Empty()
+  )
+  (
+    (a, b) => f(a) ? Stream(() => a, b) : b()
+  )
+  (
+    as
+  )
+
+  console.log('filter',
+  filter((a: number) => a > 2)(Stream(() => 1, () => Stream(() => 2, () => Stream(() => 3, () => Stream(() => 1, () => Empty())))))().head()
+  )
+  
+  const append = <A>(newas:Stream<A>) => (as: Stream<A>): () => Stream<A> => foldRight<A, Stream<A>>(
+    () => as
+  )
+  (
+    (a, b) => Stream(() => a, b)
+  )
+  (
+    newas
+  )
+
+  console.log('append',
+  append(Stream<number>(() => 5, () => Empty()))(Stream<number>(() => 2, () => Empty()))().head()
+  )
+  
+  // const flatMap = <A, B>(f:(_:A) => Stream<B>) => (as: Stream<A>): () => Stream<B> => foldRight<A, Stream<B>>(
+  //   () => Empty()
+  // )
+  // (
+  //   (a, b) => Stream(() => f(a).head(), () => Stream(f(a).tail()., b))
+  // )
+  // (
+  //   as
+  // )
+
+  // const flatmappa: () => Stream<number> = flatMap((a: number) => Stream(() => a + 1, () => Stream(() => a + 1, () => Empty())))(Stream(() => 1, () => Empty()))
+  // map((a: number) => {
+  //   console.log('flatmappp thta', a)
+  //   return a
+  // })(flatmappa())
+  // console.log(flatmappa())
+  // console.log(flatmappa().head())
+  // console.log('stream', toList(flatmappa()))
+
 }
